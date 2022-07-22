bfd7b50cc72b2a5c6b1d3eddda742d75e763235d
julio <jnandrade@uc.cl>
2022-07-22 19:54:30 -0400

    Single Page Applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,19 +4,32 @@ import Home from "./HomeComponent";
 import Menu from "./MenuComponent";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
+import Contact from "./ContactComponent";
 import { DISHES } from "../shared/dishes";
+import { COMMENTS } from "../shared/comments";
+import { PROMOTIONS } from "../shared/promotions";
+import { LEADERS } from "../shared/leaders";
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
       dishes: DISHES,
+      leaders: LEADERS,
+      promotions: PROMOTIONS,
+      comments: COMMENTS,
     };
   }
 
   render() {
     const HomePage = () => {
-      return <Home />;
+      return (
+        <Home
+          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
+      );
     };
 
     return (
@@ -30,7 +43,7 @@ class Main extends Component {
             element={<Menu dishes={this.state.dishes} />}
           />
           <Route path="/" element={<Navigate replace to="/home" />} />
-          <Route path="/contactus" element={<Navigate replace to="/home" />} />
+          <Route exact path="/contactus" element={<Contact />} />
           <Route path="/aboutus" element={<Navigate replace to="/home" />} />
         </Routes>
         <Footer />
